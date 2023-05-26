c16ca437fadde0f2c52d2ee3d4a09ab16a8c2b99
David Olanrewaju <davidolanrewaju538@gmail.com>
2023-05-26 12:53:04 +0100

    Connect Redux to React

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import bookReducer from './books/bookSlice';
-import categoriesSlice from './categories/categoriesSlice';
+import categoriesReducer from './categories/categoriesSlice';
 
 const store = configureStore({
   reducer: {
     book: bookReducer,
-    category: categoriesSlice,
+    category: categoriesReducer,
   },
 });
 
