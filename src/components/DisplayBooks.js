c1646321abac2460570088209fe5efc2c6ee8c83
David Olanrewaju <davidolanrewaju538@gmail.com>
2023-05-26 12:58:39 +0100

    Connect Redux to React

--- src/components/DisplayBooks.js
+++ src/components/DisplayBooks.js
@@ -1,4 +1,3 @@
-// import React, { useState } from 'react';
 import AddBook from './AddBook';
 import BookList from './BookList';
 
@@ -9,35 +8,3 @@ const DisplayBooks = () => (
   </div>
 );
 export default DisplayBooks;
-//   const [books, setBooks] = useState([
-//     {
-//       id: 1,
-//       title: 'Hunger Games',
-//       author: 'Suzanne Collins',
-//     },
-//     {
-//       id: 2,
-//       title: 'Dune',
-//       author: 'Frank Herbert',
-//     },
-//     {
-//       id: 3,
-//       title: 'Harry Potter',
-//       author: 'J.K Rowling',
-//     },
-//   ]);
-
-// const delBook = (id) => {
-//   setBooks([
-//     ...books.filter((book) => book.id !== id),
-//   ]);
-// };
-
-// const addBook = (title, author) => {
-//   const newBook = {
-//     id: books.length + 1,
-//     title,
-//     author,
-//   };
-//   setBooks([...books, newBook]);
-// };
